0fe97cb3755f7b6b7330d6feee0fbf012fd4a809
Martin <martouser@abv.bg>
2023-06-15 19:57:39 +0300

    Add project files.

---
+++ OOP - Inheritance/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OOP___Inheritance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
