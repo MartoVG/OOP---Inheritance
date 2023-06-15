35361c16ca2df20717701a90b98e2bb4a4461f8b
Martin <martouser@abv.bg>
2023-06-15 21:46:49 +0300

    Inheritances

---
+++ Hierarchical Inheritance/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Hierarchical_Inheritance
+{
+    internal class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Dog sharo = new Dog();
+            sharo.Eat();
+            sharo.Bark();
+
+            Cat maca = new Cat();
+            maca.Eat();
+            maca.Meow();
+        }
+    }
+}
---
+++ Single Inheritance/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Single_Inheritance
+{
+    internal class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Dog dog = new Dog();
+            dog.Eat();
+            dog.Bark();
+        }
+    }
+}
